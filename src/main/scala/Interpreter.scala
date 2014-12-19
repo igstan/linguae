a445793d5e7a970361b15f7d7c920976716f4419
Ionuț G. Stan <ionut.g.stan@gmail.com>
2014-12-20 01:20:58 +0200

    [lingua-003] Extract helper method for arithmetic expressions

--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -3,19 +3,16 @@ package toy
 object Interpreter {
   def eval(ast: Node): Value = ast match {
     case Num(n) => Value.Num(n)
-    case Add(l, r) =>
-      eval(l) match {
-        case Value.Num(l) =>
-          eval(r) match {
-            case Value.Num(r) => Value.Num(l + r)
-          }
-      }
-    case Sub(l, r) =>
-      eval(l) match {
-        case Value.Num(l) =>
-          eval(r) match {
-            case Value.Num(r) => Value.Num(l - r)
-          }
-      }
+    case Add(l, r) => arith(l, r, _ + _)
+    case Sub(l, r) => arith(l, r, _ - _)
+  }
+
+  private def arith(l: Node, r: Node, op: (Int, Int) => Int): Value = {
+    eval(l) match {
+      case Value.Num(l) =>
+        eval(r) match {
+          case Value.Num(r) => Value.Num(op(l, r))
+        }
+    }
   }
 }
