4306ee1266df2eb9c0f09f7bfc5bfa91fefaa29f
Ionuț G. Stan <ionut.g.stan@gmail.com>
2014-12-20 02:58:41 +0200

    [lingua-003] Make Result a monad

--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -1,32 +1,25 @@
 package toy
 
 object Interpreter {
-  def eval(ast: Node, env: Environment): Result = ast match {
+  def eval(ast: Node, env: Environment): Result[Value] = ast match {
     case Num(n) => Result.Success(Value.Num(n))
     case Add(l, r) => arith(l, r, env, _ + _)
     case Sub(l, r) => arith(l, r, env, _ - _)
     case Mul(l, r) => arith(l, r, env, _ * _)
     case Div(l, r) => arith(l, r, env, _ / _)
     case If(cond, yes, no) =>
-      eval(cond, env) match {
-        case f: Result.Failure => f
-        case Result.Success(Value.Num(0)) => eval(no, env)
-        case Result.Success(Value.Num(_)) => eval(yes, env)
-        case Result.Success(_: Value.Fun) =>
-          Result.Failure("If condition was not a number, but a function.")
+      eval(cond, env).flatMap {
+        case Value.Num(0) => eval(no, env)
+        case Value.Num(_) => eval(yes, env)
+        case _: Value.Fun => Result.Failure("If condition was not a number, but a function.")
       }
     case Fun(param, body) => Result.Success(Value.Fun(param, body, closure = env))
     case App(fn, arg) =>
-      eval(fn, env) match {
-        case f: Result.Failure => f
-        case Result.Success(Value.Num(_)) =>
-          Result.Failure("Number in function application position.")
-        case Result.Success(Value.Fun(param, body, closure)) =>
-          eval(arg, env) match {
-            case f: Result.Failure => f
-            case Result.Success(value) =>
-              val augmentedEnv = closure.set(param, value)
-              eval(body, augmentedEnv)
+      eval(fn, env).flatMap {
+        case Value.Num(_) => Result.Failure("Number in function application position.")
+        case Value.Fun(param, body, closure) =>
+          eval(arg, env).flatMap {
+            case value => eval(body, closure.set(param, value))
           }
       }
     case Ref(name) =>
@@ -38,18 +31,13 @@ object Interpreter {
       eval(desugared, env)
   }
 
-  private def arith(l: Node, r: Node, env: Environment, op: (Int, Int) => Int): Result = {
-    eval(l, env) match {
-      case f: Result.Failure => f
-      case Result.Success(_: Value.Fun) =>
-        Result.Failure("Left operand is not a number")
-      case Result.Success(Value.Num(l)) =>
-        eval(r, env) match {
-          case f: Result.Failure => f
-          case Result.Success(_: Value.Fun) =>
-            Result.Failure("Right operand is not a number")
-          case Result.Success(Value.Num(r)) =>
-            Result.Success(Value.Num(op(l, r)))
+  private def arith(l: Node, r: Node, env: Environment, op: (Int, Int) => Int): Result[Value] = {
+    eval(l, env).flatMap {
+      case _: Value.Fun => Result.Failure("Left operand is not a number")
+      case Value.Num(l) =>
+        eval(r, env).flatMap {
+          case _: Value.Fun => Result.Failure("Right operand is not a number")
+          case Value.Num(r) => Result.Success(Value.Num(op(l, r)))
         }
     }
   }
--- src/main/scala/Result.scala
+++ src/main/scala/Result.scala
@@ -1,7 +1,24 @@
 package toy
 
-sealed trait Result
+sealed trait Result[+A] {
+  import Result._
+
+  def map[B](fn: A => B): Result[B] = {
+    this match {
+      case Success(s) => Success(fn(s))
+      case Failure(f) => Failure(f)
+    }
+  }
+
+  def flatMap[B](fn: A => Result[B]): Result[B] = {
+    this match {
+      case Success(s) => fn(s)
+      case Failure(f) => Failure(f)
+    }
+  }
+}
+
 object Result {
-  case class Success(value: Value) extends Result
-  case class Failure(reason: String) extends Result
+  case class Success[A](value: A) extends Result[A]
+  case class Failure(reason: String) extends Result[Nothing]
 }
