efec4539125c1b35a41a2b6d83cf8d8e4461c406
Ionuț G. Stan <ionut.g.stan@gmail.com>
2015-01-19 11:06:48 +0200

    [lingua-004] Move all state transition methods to State

--- src/main/scala/reader.scala
+++ src/main/scala/reader.scala
@@ -19,99 +19,20 @@ case class UnmatchedRightParen(row: Int, col: Int) extends RuntimeException
  * The reader itself!
  */
 class Reader(traceExecution: Boolean) {
-  type Transition = State => State
-
   def read(source: String): List[NODE] = {
-    val finalState = commitAtom(source.foldLeft(State.empty)(transition))
-    verifyUnmatchedParenthesis(finalState)
-    finalState.program.toList
+    source.foldLeft(State.empty)(fold)
+      .commitAtom
+      .verifyUnmatchedParenthesis()
+      .program
+      .toList
   }
 
-  def verifyUnmatchedParenthesis(state: State): Unit = {
-    if (state.lastList.isDefined) {
-      state.listPositions match {
-        case Nil => sys.error("this is a bug")
-        case (row, col) :: _ => throw UnmatchedLeftParen(row, col)
-      }
-    }
-  }
-
-  def transition(state: State, char: Char) = {
-    adjustPosition(char) {
-      char match {
-        case '('                            => startList(commitAtom(state))
-        case ')'                            => commitList(commitAtom(state))
-        case c if Character.isWhitespace(c) => commitAtom(state)
-        case c                              => adjustAtom(c)(state)
-      }
-    }
-  }
-
-  def adjustPosition(char: Char): Transition = { state =>
-    if (char == '\n') {
-      state.copy(col = 1, row = state.row + 1)
-    } else {
-      state.copy(col = state.col + 1)
-    }
-  }
-
-  def log(message: String)(transition: Transition): Transition = { state =>
-    val changedState = transition(state)
-    if (traceExecution) println(s"$message: $changedState")
-    changedState
-  }
-
-  val commitList: Transition = log("commitList") { state =>
-    state.lastList match {
-      case None => throw UnmatchedRightParen(state.row, state.col)
-      case Some(list) =>
-        state.lists match {
-          case Vector() =>
-            state.copy(
-              program = state.program :+ LIST(list.toList),
-              lastList = None,
-              listPositions = state.listPositions.tail
-            )
-          case prevLists :+ x =>
-            state.copy(
-              lastList = Some(x :+ LIST(list.toList)),
-              lists = prevLists,
-              listPositions = state.listPositions.tail
-            )
-        }
-    }
-  }
-
-  val startList: Transition = log("startList") { state =>
-    state.lastList match {
-      case None => state.copy(
-        lastList = Some(Vector.empty),
-        listPositions = (state.row -> state.col) :: state.listPositions
-      )
-      case Some(list) => state.copy(
-        lastList = Some(Vector.empty),
-        lists = state.lists :+ list,
-        listPositions = (state.row -> state.col) :: state.listPositions
-      )
-    }
-  }
-
-  val commitAtom: Transition = log("commitAtom") { state =>
-    state.lastAtom match {
-      case None => state
-      case Some(atom) =>
-        val commited = state.lastList match {
-          case None => state.copy(program = state.program :+ ATOM(atom))
-          case Some(list) => state.copy(lastList = Some(list :+ ATOM(atom)))
-        }
-        commited.copy(lastAtom = None)
-    }
-  }
-
-  def adjustAtom(char: Char): Transition = log(s"adjustAtom($char)") { state =>
-    state.lastAtom match {
-      case None => state.copy(lastAtom = Some(char.toString))
-      case Some(atom) => state.copy(lastAtom = Some(atom + char))
+  def fold(state: State, c: Char) = {
+    c match {
+      case '('                            => state.commitAtom.startList.adjustPosition(c)
+      case ')'                            => state.commitAtom.commitList.adjustPosition(c)
+      case c if Character.isWhitespace(c) => state.commitAtom.adjustPosition(c)
+      case c                              => state.adjustAtom(c).adjustPosition(c)
     }
   }
 
@@ -124,6 +45,84 @@ class Reader(traceExecution: Boolean) {
     row: Int,
     col: Int
   ) {
+    def adjustPosition(char: Char): State = {
+      if (char == '\n') {
+        copy(col = 1, row = row + 1)
+      } else {
+        copy(col = col + 1)
+      }
+    }
+
+    def commitList: State = log("commitList") {
+      lastList match {
+        case None => throw UnmatchedRightParen(row, col)
+        case Some(list) =>
+          lists match {
+            case Vector() =>
+              copy(
+                program = program :+ LIST(list.toList),
+                lastList = None,
+                listPositions = listPositions.tail
+              )
+            case prevLists :+ x =>
+              copy(
+                lastList = Some(x :+ LIST(list.toList)),
+                lists = prevLists,
+                listPositions = listPositions.tail
+              )
+          }
+      }
+    }
+
+    def startList: State = log("startList") {
+      lastList match {
+        case None => copy(
+          lastList = Some(Vector.empty),
+          listPositions = (row -> col) :: listPositions
+        )
+        case Some(list) => copy(
+          lastList = Some(Vector.empty),
+          lists = lists :+ list,
+          listPositions = (row -> col) :: listPositions
+        )
+      }
+    }
+
+    def commitAtom: State = log("commitAtom") {
+      lastAtom match {
+        case None => this
+        case Some(atom) =>
+          val commited = lastList match {
+            case None => copy(program = program :+ ATOM(atom))
+            case Some(list) => copy(lastList = Some(list :+ ATOM(atom)))
+          }
+          commited.copy(lastAtom = None)
+      }
+    }
+
+    def adjustAtom(char: Char): State = log(s"adjustAtom($char)") {
+      lastAtom match {
+        case None => copy(lastAtom = Some(char.toString))
+        case Some(atom) => copy(lastAtom = Some(atom + char))
+      }
+    }
+
+    def verifyUnmatchedParenthesis(): State = {
+      if (lastList.isDefined) {
+        listPositions match {
+          case Nil => sys.error("this is a bug")
+          case (row, col) :: _ => throw UnmatchedLeftParen(row, col)
+        }
+      }
+
+      this
+    }
+
+    private def log[A](message: String)(result: A): A = {
+      if (traceExecution) println(s"$message: $result")
+      result
+    }
+
     override def toString = {
       s"""State(
       |       program = $program,
