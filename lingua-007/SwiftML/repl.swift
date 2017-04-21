e5a262dc7bbf37e9a334a9503f634934db30fff7
Ionuț G. Stan <ionut.g.stan@gmail.com>
2017-04-21 16:31:48 +0300

    [lingua-007] Use recursion instead of while loop

--- lingua-007/SwiftML/repl.swift
+++ lingua-007/SwiftML/repl.swift
@@ -3,19 +3,19 @@
 //
 
 func repl(prompt: String) {
-  repeat {
-    print(prompt, terminator: "")
-    switch readLine() {
-      case .none: return
-      case .some(":q"), .some(":quit"):
-        print("Bye!")
-        return
-      case .some(let source):
-        switch parse(scanAll(source.characters)) {
-          case .none: print("syntax error")
-          case let .some((term, _)):
-            print("term:", String(describing: term.eval(env: [:])))
-        }
-    }
-  } while (true)
+  print(prompt, terminator: "")
+
+  switch readLine() {
+    case .none, .some(":q"), .some(":quit"):
+      print("Bye!")
+      return
+    case .some(let source):
+      switch parse(scanAll(source.characters)) {
+        case .none:
+          print("syntax error")
+        case let .some((term, _)):
+          print("val it =", String(describing: term.eval(env: [:])))
+      }
+      repl(prompt: prompt)
+  }
 }
