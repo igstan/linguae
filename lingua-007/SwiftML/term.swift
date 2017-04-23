b05a8e3f3996b47a7067aaf06f8d5285885a586f
Ionuț G. Stan <ionut.g.stan@gmail.com>
2017-04-23 18:31:13 +0300

    [lingua-007] Move the Value enum into its own file

--- lingua-007/SwiftML/term.swift
+++ lingua-007/SwiftML/term.swift
@@ -2,26 +2,6 @@
 // SwiftML                                                                    //
 // -------------------------------------------------------------------------- //
 
-indirect enum Value {
-  case Num(Int)
-  case Bool(Bool)
-  case Fun(String, Term, [String:Value])
-  case Primitive((Value) -> Result<Value, String>)
-
-  func success() -> Result<Value, String> {
-    return .Success(self)
-  }
-
-  func toString() -> String {
-    switch self {
-      case .Num(let n): return String(n)
-      case .Bool(true): return "true"
-      case .Bool(false): return "false"
-      case .Fun(_, _, _), .Primitive(_): return "fn"
-    }
-  }
-}
-
 indirect enum Term {
   case Num(Int)
   case Var(String)
