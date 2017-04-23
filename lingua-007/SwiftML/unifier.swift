2e407305621bcd9defcb52ee255ca0eeeacdfc1d
Ionuț G. Stan <ionut.g.stan@gmail.com>
2017-04-23 18:11:20 +0300

    [lingua-007] Replace `switch` statement with `guard`

--- lingua-007/SwiftML/unifier.swift
+++ lingua-007/SwiftML/unifier.swift
@@ -4,15 +4,15 @@
 
 enum Unifier {
   static func solve(constraints: Set<Constraint>) -> Result<Substitution, String> {
-    switch constraints.first {
-      case .none: return .Success(Substitution.empty)
-      case .some(let constraint):
-        return constraint.solve().flatMap { substHead in
-          let tail = substHead.applyTo(constraints: Set(constraints.dropFirst()))
-          return solve(constraints: tail).map { substTail in
-            return substHead.compose(with: substTail)
-          }
-        }
+    guard let constraint = constraints.first else {
+      return .Success(Substitution.empty)
+    }
+
+    return constraint.solve().flatMap { substHead in
+      let tail = substHead.applyTo(constraints: Set(constraints.dropFirst()))
+      return solve(constraints: tail).map { substTail in
+        return substHead.compose(with: substTail)
+      }
     }
   }
 }
