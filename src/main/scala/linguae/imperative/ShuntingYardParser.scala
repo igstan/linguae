1b01469d7dd162f9673b5903f3e66519281d2c61
Ionuț G. Stan <ionut.g.stan@gmail.com>
2022-03-31 19:08:43 +0300

    [lingua-009] Write an imperative version of the Shunting Yard algorithm

---
+++ src/main/scala/linguae/imperative/ShuntingYardParser.scala
@@ -0,0 +1,63 @@
+package linguae
+package imperative
+
+import scala.collection.mutable
+
+final case class ShuntingYardParser(operatorTable: Map[String, Fixity]) {
+
+  def parse(tokens: List[Token]): Expr = {
+    val toknStack = mutable.Stack.from(tokens)
+    val exprStack = mutable.Stack.empty[Expr]
+    val operStack = mutable.Stack.empty[String]
+
+    def applyOperator() = {
+      val op = operStack.pop()
+      val a1 = exprStack.pop()
+      val a0 = exprStack.pop()
+      exprStack.push(Expr.BinOp(op, a0, a1))
+    }
+
+    while (toknStack.nonEmpty) {
+      toknStack.pop() match {
+        case Token.Const(const) => exprStack.push(Expr.Const(const))
+        case Token.LParen => operStack.push("(")
+
+        case Token.RParen =>
+          while (operStack.nonEmpty && operStack.top != "(") {
+            applyOperator()
+          }
+
+          if (operStack.top == "(") operStack.pop()
+          else sys.error("missing open parenthesis")
+
+        case Token.BinOp(binOp) =>
+          val binOpFixity = operatorTable(binOp)
+          var pendingOperators = true
+
+          while (pendingOperators) {
+            if (operStack.isEmpty || operStack.top == "(") {
+              pendingOperators = false
+            } else {
+              val topOpFixity = operatorTable(operStack.top)
+
+              if (binOpFixity.appliesAfter(topOpFixity)) {
+                applyOperator()
+              } else {
+                pendingOperators = false
+              }
+            }
+          }
+
+          operStack.push(binOp)
+      }
+    }
+
+    while (operStack.nonEmpty) {
+      applyOperator()
+    }
+
+    exprStack
+      .pop()
+      .ensuring(exprStack.isEmpty, s"non-empty expression stack: $exprStack")
+  }
+}
---
+++ src/test/scala/linguae/imperative/test/ShuntingYardParserSuite.scala
@@ -0,0 +1,39 @@
+package linguae
+package imperative
+package test
+
+final class ShuntingYardParserSuite extends munit.FunSuite {
+  private val operatorTable = Map(
+    "^" -> Fixity.R(10),
+    "*" -> Fixity.L(5),
+    "/" -> Fixity.L(5),
+    "+" -> Fixity.L(0),
+    "-" -> Fixity.L(0),
+  )
+
+  private val parser = ShuntingYardParser(operatorTable)
+
+  private val exprs = List(
+    "1 + 2",
+    "(1 + 2) * 3",
+    "4 + -2 - 1",
+    "4 * 2 / 1",
+    "1 + 2 * 3",
+    "4 + 4 * 2 / (1 - 5)",
+    "4 + (4 + 2) / (1 - 5)",
+  )
+
+  exprs.foreach { expr =>
+    test(s"parses: $expr") {
+      val result = Token
+        .parser
+        .consume(expr)
+        .map(parser.parse)
+        .toOption
+        .get
+        .pretty(operatorTable)
+
+      assertEquals(result, expr)
+    }
+  }
+}
