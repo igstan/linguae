439c9e56c8af670928afbdde95a468855baedbb3
Ionuț G. Stan <ionut.g.stan@gmail.com>
2022-03-31 19:28:27 +0300

    [lingua-009] Remove duplication in tests by extracting an abstract test suite

---
+++ src/test/scala/linguae/test/ExprParserSuite.scala
@@ -0,0 +1,38 @@
+package linguae
+package test
+
+abstract class ExprParserSuite extends munit.FunSuite {
+  def parser: ExprParser
+
+  protected val operatorTable = Map(
+    "^" -> Fixity.R(10),
+    "*" -> Fixity.L(5),
+    "/" -> Fixity.L(5),
+    "+" -> Fixity.L(0),
+    "-" -> Fixity.L(0),
+  )
+
+  private val exprs = List(
+    "1 + 2",
+    "(1 + 2) * 3",
+    "4 + -2 - 1",
+    "4 * 2 / 1",
+    "1 + 2 * 3",
+    "4 + 4 * 2 / (1 - 5)",
+    "4 + (4 + 2) / (1 - 5)",
+  )
+
+  exprs.foreach { expr =>
+    test(s"parses: $expr") {
+      val result = Token
+        .parser
+        .consume(expr)
+        .map(parser.parse)
+        .toOption
+        .get
+        .pretty(operatorTable)
+
+      assertEquals(result, expr)
+    }
+  }
+}
--- src/test/scala/linguae/test/ImperativeShuntingYardParserSuite.scala
+++ src/test/scala/linguae/test/ImperativeShuntingYardParserSuite.scala
@@ -1,38 +1,7 @@
 package linguae
 package test
 
-final class ImperativeShuntingYardParserSuite extends munit.FunSuite {
-  private val operatorTable = Map(
-    "^" -> Fixity.R(10),
-    "*" -> Fixity.L(5),
-    "/" -> Fixity.L(5),
-    "+" -> Fixity.L(0),
-    "-" -> Fixity.L(0),
-  )
-
-  private val parser = ImperativeShuntingYardParser(operatorTable)
-
-  private val exprs = List(
-    "1 + 2",
-    "(1 + 2) * 3",
-    "4 + -2 - 1",
-    "4 * 2 / 1",
-    "1 + 2 * 3",
-    "4 + 4 * 2 / (1 - 5)",
-    "4 + (4 + 2) / (1 - 5)",
-  )
-
-  exprs.foreach { expr =>
-    test(s"parses: $expr") {
-      val result = Token
-        .parser
-        .consume(expr)
-        .map(parser.parse)
-        .toOption
-        .get
-        .pretty(operatorTable)
-
-      assertEquals(result, expr)
-    }
-  }
+final class ImperativeShuntingYardParserSuite extends ExprParserSuite {
+  override def parser: ExprParser =
+    ImperativeShuntingYardParser(operatorTable)
 }
