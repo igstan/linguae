76e6e0bf3e40aa4155d39282832ea308dbecdb03
Ionuț G. Stan <ionut.g.stan@gmail.com>
2022-03-31 19:52:27 +0300

    [lingua-009] Convert the `Token` ADT to an `enum`

--- src/main/scala/linguae/Token.scala
+++ src/main/scala/linguae/Token.scala
@@ -1,34 +1,34 @@
 package linguae
 
-sealed trait Token extends Product with Serializable
+enum Token {
+  case Const(value: Int)
+  case BinOp(value: String)
+  case LParen
+  case RParen
+}
 
 object Token {
-  final case class Const(value: Int) extends Token
-  final case class BinOp(value: String) extends Token
-  case object LParen extends Token
-  case object RParen extends Token
-
   def parser: Parser[List[Token]] = {
     import Parser.*
 
     def token[A](parser: Parser[A]): Parser[A] = spaces *> parser <* spaces
     def spaces: Parser[Unit] = satisfy(_.isWhitespace).optionalMany.void
-    def lparen: Parser[Token] = char('(').as(Token.LParen)
-    def rparen: Parser[Token] = char(')').as(Token.RParen)
+    def lparen: Parser[Token] = char('(').as(LParen)
+    def rparen: Parser[Token] = char(')').as(RParen)
 
     def number: Parser[Token] =
       for {
         n <- char('-').optional.map(_.isDefined)
         d <- satisfy(_.isDigit).many.map(_.mkString.toInt)
       } yield {
-        Token.Const(if (n) -d else d)
+        Const(if (n) -d else d)
       }
 
     def operator: Parser[Token] =
       Set('+', '-', '*', '/', '^')
         .map(char)
         .reduce(_ | _)
-        .map(op => Token.BinOp(op.toString))
+        .map(op => BinOp(op.toString))
 
     token(lparen | rparen | number | operator).optionalMany
   }
