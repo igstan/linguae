ebc725bd77894d11148e7feada8bd6078f2360c9
Ionuț G. Stan <ionut.g.stan@gmail.com>
2023-04-07 21:58:23 +0300

    [lingua-007] Fix build of SwiftML with latest Swift version
    See also: https://forums.swift.org/t/circular-reference-in-enums/64267

--- lingua-007-µml-swift/SwiftML/constrain.swift
+++ lingua-007-µml-swift/SwiftML/constrain.swift
@@ -2,42 +2,6 @@
 // SwiftML                                                                    //
 // -------------------------------------------------------------------------- //
 
-enum Constraint: Hashable, CustomStringConvertible {
-  case Equal(Type, Type)
-
-  static func ==(lhs: Constraint, rhs: Constraint) -> Bool {
-    switch (lhs, rhs) {
-      case let (Equal(a1, b1), Equal(a2, b2)): return a1 == a2 && b1 == b2
-    }
-  }
-
-  func hash(into hasher: inout Hasher) {
-    switch self {
-      case let .Equal(r, p):
-        hasher.combine(r)
-        hasher.combine(p)
-    }
-  }
-
-  var description: String {
-    switch self {
-      case let .Equal(a, b): return "\(a) ≣ \(b)"
-    }
-  }
-
-  func solve() -> Result<Substitution, TypeError> {
-    switch self {
-      case .Equal(.Int, .Int): return .Success(Substitution.empty)
-      case .Equal(.Bool, .Bool): return .Success(Substitution.empty)
-      case let .Equal(.Fun(p1, r1), .Fun(p2, r2)):
-        return Unifier.solve(constraints: [.Equal(p1, p2), .Equal(r1, r2)])
-      case let .Equal(.Var(v), type): return type.solve(tvar: v)
-      case let .Equal(type, .Var(v)): return type.solve(tvar: v)
-      case let .Equal(a, b): return .Failure(.Conflict(a, b))
-    }
-  }
-}
-
 func constrain<Attr>(_ term: Term<(Attr, Type)>) -> Set<Constraint> {
   switch term {
     case .Var(_, _): return []
